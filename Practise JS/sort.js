a32eeda310f73fdfccf7a7302d95dcaacc4e6173
Kennedy Gitonga <66670644+Kenato254@users.noreply.github.com>
2021-10-20 01:06:10 +0300

    Initial Commit

---
+++ Practise JS/sort.js
@@ -0,0 +1,128 @@
+/* Sorting Array Method Sort & Reverse */
+/* String */
+function sortString() {    
+    const fruitsTypes = [
+        'Pears', 
+        'Citrus', 
+        'Stone fruit', 
+        'Tropical and exotic', 
+        'Berries', 
+        'Melons', 
+        'Tomatoes and avocados'
+    ];
+
+    fruitsTypes.sort(); // Sorts alphabetically
+    fruitsTypes.reverse(); // Reverse order of the elements
+
+    /* Sort array by length of elements */
+    fruitsTypes.sort(function(a, b){return a.length - b.length}); 
+}
+/* Numerics */
+function sortNumeric() {
+    /* Numeric Sort Using Compare function */
+    const points = [40, 100, 1, 5, 25, 10];
+    points.sort(function(a, b){return a - b}); // Ascending
+    points.sort(function(a, b){return b - a}); // Descending
+}
+
+/* The Fisher Yates shuffle Method Randon Order*/
+ function fisherYates(arr) {
+    for (let x = nums.length -1; x > 0; x--) {
+        let y = Math.floor(Math.random() * x);
+        let z = nums[x];
+        nums[x] = nums[y];
+        nums[y] = z;
+    }
+    return arr;
+}
+
+/* Custom Sorting Function using Swapping variable Method*/
+const nums = [40, 100, 1, 80, 5, 25, 10, 45];
+function sortArray(arr) {
+    let sort = true;
+    while (sort) {
+        sort = false;
+        for (let x = 0; x < arr.length-1; x++) {
+            if (arr[x] > arr[x + 1]) {
+                sort = true;
+                arr[x] = arr[x] ^ arr[x + 1];
+                arr[x + 1] = arr[x] ^ arr[x + 1];
+                arr[x] = arr[x] ^ arr[x + 1];
+            }
+        }
+    }
+    return arr;
+}
+
+/* Find the highest/Lowest Array Value */
+
+//Highest value using Math.max.apply
+function findMax(arr) {
+    return Math.max.apply(null, arr);
+}
+
+// Loweest value using Math.min.apply
+function findMin(arr) {
+    return Math.min.apply(null, arr);
+}
+
+// Custom Find Max value Function
+function findHighest(arr) {
+    let len = arr.length;
+    let max = -Infinity;
+    while(len--) {
+        if (arr[len] > max) {
+            max = arr[len];
+        }
+    }
+    return max;
+}
+// Custom Find Min value Function
+function findLowest(arr) {
+    let len = arr.length;
+    let min = Infinity;
+    while(len--) {
+        if (arr[len] < min) {
+            min = arr[len];
+        }
+    }
+    return min;
+}
+
+// Find the second Highest. A TOUGH ONE
+function secondHighest(arr) {
+    let len = arr.length;
+    let max = -Infinity;
+    let sndMax = -Infinity;
+    while (len--) {
+        if (arr[len] > max) {
+            max = arr[len];
+            sndMax = max;
+            console.log(`Max: ${max}`);
+        }
+        else if ((arr[len] > sndMax) && (arr[len] < max)){
+            sndMax = arr[len];
+            console.log(`Second Max: ${sndMax}`);
+            console.log('------------------');
+        }
+    }
+    return
+}
+/* Sorting OBJECT Arrays */
+const cars = [
+    {type:"Volvo", year:2016},
+    {type:"Saab", year:2001},
+    {type:"BMW", year:2010}
+];
+// Sorting using Numeric in the object
+// cars.sort(function(a, b){return a.year - b.year});
+
+// Sorting using String in the object
+cars.sort(function(a, b) {
+    let x = a.type.toLowerCase();
+    let y = b.type.toLowerCase();
+    if (x < y) {return -1;}
+    if (x > y) {return 1;}
+    return 0;
+});
+
---
+++ README.md
@@ -0,0 +1,8 @@
+# 100 Days Of Code - Log
+## This is my first round of 100DaysOfCode Challege. Entails, minimal of 1 hour of coding Monday to Sunday from 19th, Oct 2021 to 27th, Jan 2022.
+## JavaScript.
+<a name="toc"></a>
+### Table of Contents
+|Day|Topic|Day|Topic
+|:---:|:-----:|:---:|:-----:|
+|[Day 1](#day-1) **19th Oct 2021**|Basics in javascript:Array-Sorting an Array, Reversing an Array, Numeric Sort, The Compare Function, The Fisher Yates Method, Using Math.min() on an Array, Custom Min/Max and Sorting Object Arrays.
