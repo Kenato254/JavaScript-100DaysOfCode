e143e7b5709bceb812b3186d56be792d514012f8
Kennedy Gitonga <66670644+Kenato254@users.noreply.github.com>
2021-11-05 00:56:44 +0300

    Master (#23)
    * Debugging Style Guide & Best Practices
    
    * README.md Update
    
    * JS Getter and Setter, CSS W3School Card Clone
    
    * Change Directory
    
    * README.md
    
    * README.md Update
    
    * README.md
    
    * JS Promises & Map () Objects

---
+++ Practice_JS/promise.js
@@ -0,0 +1,33 @@
+"use strict";
+
+/* Promise ES6 */
+let skippedADay = true;
+let didMy100DaysOFCode = '#100DaysOfCode';
+const promise = new Promise((resolve, reject) => {
+    if (!skippedADay) {
+        resolve("Good work keep working hard!");
+    } else {
+        reject("Work load doubled. No day off!")
+    }
+})
+promise.then((message) => {
+    console.log("One day less of " + didMy100DaysOFCode + ". " + message);
+}).catch((message) => {
+    console.log("One day added to " + didMy100DaysOFCode + ". " + message);
+})
+
+/* Map() ES6 */
+function myMap() {
+    const fruits = new Map();
+    fruits.set('Apples', 500);
+    fruits.set('Bananas', 100);
+    fruits.set('Cider', 200);
+    fruits.set('Oranges', 900);
+    fruits.set('Kiwi', 600);
+    return fruits;
+}
+// console.log(myMap().get('Bananas'));
+// console.log(myMap().keys());
+// console.log(myMap().values());
+// console.log(myMap().size);
+// console.log(myMap().clear());
