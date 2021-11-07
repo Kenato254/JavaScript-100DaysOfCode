52aab2266c148bccea64cc314b4eb53e09ca0c37
Kenato254 <kenedygitonga@gmail.com>
2021-11-08 01:39:49 +0300

    Tic Tac Toe

---
+++ Tic-Tac-Toe/script.js
@@ -0,0 +1,66 @@
+const X_CLASS = 'x';
+const CIRCLE_CLASS = 'circle';
+const WINNING_COMBINATIONS = [
+    // Horizontal Win
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    // Vertical Win
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    // Diagonal Win
+    [0, 4, 8],
+    [2, 4, 6]
+];
+const cellElements = document.querySelectorAll('[data-cell]');
+const board = document.getElementById('board');
+let circleTurn;
+
+startGame()
+
+function startGame() {
+    circleTurn = false;
+    cellElements.forEach(cell => {
+        cell.addEventListener('click', handleClick, { once: true })
+    })
+    setBoardHoverClass()
+}
+
+function handleClick(e) {
+    const cell = e.target;
+    const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS;
+    placeMark(cell, currentClass)
+    if (checkWin(currentClass)) {
+        console.log('Winner');
+    } else {
+        console.log('No Winner');
+    }
+    // Check For win
+    // Check for Draw
+    // Switch Turns
+    swapTurns()
+    setBoardHoverClass()
+}
+function placeMark(cell, currentClass) {
+    cell.classList.add(currentClass)
+}
+function swapTurns() {
+    circleTurn = !circleTurn;    
+}
+function setBoardHoverClass() {
+    board.classList.remove(X_CLASS);
+    board.classList.remove(CIRCLE_CLASS);
+    if (circleTurn) {
+        board.classList.add(CIRCLE_CLASS);
+    } else {
+        board.classList.add(X_CLASS);
+    }
+}
+function checkWin(currentClass) {
+    WINNING_COMBINATIONS.some(combination => {
+        return combination.every(index => {
+            return cellElements[index].classList.contains(currentClass);
+        })
+    })
+}
